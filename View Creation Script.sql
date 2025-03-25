2a6836225fe58ee0f0bf883c6ff183a3735151c7
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2025-03-25 16:27:15 -0400

    Update View Creation Script.sql

--- View Creation Script.sql
+++ View Creation Script.sql
@@ -1,3 +1,41 @@
+CREATE VIEW `view_customers` AS
+  SELECT *
+  FROM `customers`;
+
+CREATE VIEW `view_items` AS
+  SELECT *
+  FROM `menuitem`;
+
+CREATE VIEW `view_locations` AS
+  SELECT *
+  FROM `restaurants`;
+
+CREATE VIEW `view_orders` AS
+  SELECT *
+  FROM `orders`;
+
+CREATE VIEW `view_payments` AS
+  SELECT *
+  FROM `payments`;
+
+CREATE VIEW `view_promotions` AS
+  SELECT *
+  FROM `promotions`;
+
+CREATE VIEW `view_reservations` AS
+  SELECT * 
+  FROM `reservations`;
+
+CREATE VIEW `view_tables` AS
+  SELECT *
+  FROM `restaurant_tables`;
+
+CREATE TABLE `view_workers` AS
+  SELECT *
+  FROM `workers`;
+
+  
+
 SELECT * FROM restaurant_reservation.view_customers;
 SELECT * FROM restaurant_reservation.view_items;
 SELECT * FROM restaurant_reservation.view_locations;
