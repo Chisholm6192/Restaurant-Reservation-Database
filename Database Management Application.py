bcece957b186ade383ee4e0bed6716cd489a1f8b
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2025-03-26 12:11:05 -0400

    Update Database Management Application.py

--- Database Management Application.py
+++ Database Management Application.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Winterhawks1",
+    password="*******",
     database="restaurant_reservation"
 )
 
